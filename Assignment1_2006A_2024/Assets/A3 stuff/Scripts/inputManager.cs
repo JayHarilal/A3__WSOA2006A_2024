60669a6d825a36a9453d9a90758e0acdfe955dd2
JayHarilal <jjharilal@gmail.com>
2024-10-11 11:28:04 +0200

    New Input system
    works on keyboard and gamepad, has jump, sprint, crouch and walk

---
+++ Assignment1_2006A_2024/Assets/A3 stuff/Scripts/inputManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class inputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.WalkingActions walking;
+
+    private playerMovementProperties properties;
+    private playerLook look;
+
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        walking = playerInput.Walking;
+        properties = GetComponent<playerMovementProperties>();
+        look = GetComponent<playerLook>();
+        walking.Jump.performed += ctx => properties.Jump();
+        walking.Crouch.performed += ctx => properties.Crouch();
+        walking.Sprint.performed += ctx => properties.Sprint();
+    }
+
+    void FixedUpdate()
+    {
+        properties.movement(walking.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.lookAround(walking.lookAround.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        walking.Enable();
+    }
+
+    private void OnDisable()
+    {
+        walking.Disable();
+    }
+}
---
+++ Assignment1_2006A_2024/Assets/A3 stuff/Scripts/playerMovementProperties.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementProperties : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private float speed = 12f;
+    private bool isGrounded;
+    private float gravity = -9.8f;
+    public float jumpHeight = 4f;
+    bool crouching = false;
+    float crouchTimer = 1;
+    bool lerpCrouch = false;
+    bool sprinting = false;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+        if (lerpCrouch)
+        {
+            crouchTimer += Time.deltaTime;
+            float p = crouchTimer / 1;
+            p *= p;
+            if (crouching)
+                controller.height = Mathf.Lerp(controller.height, 1, p);
+            else
+                controller.height = Mathf.Lerp(controller.height, 2, p);
+            if(p>1)
+            {
+                lerpCrouch = false;
+                crouchTimer = 0f;
+            }
+            
+        }
+    }
+
+    public void movement(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if (isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2f;
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -4.0f * gravity);
+        }
+    }
+
+    public void Crouch()
+    {
+        crouching = !crouching;
+        crouchTimer = 0;
+        lerpCrouch = true;
+    }
+
+    public void Sprint()
+    {
+        sprinting = !sprinting;
+        if (sprinting)
+        {
+            speed = 8;
+        }
+        else
+            speed = 5;
+    }
+}
